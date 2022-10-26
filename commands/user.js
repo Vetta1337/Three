e922f690652d1c686ff6753c0748d3a1882c30c5
Vetta1337 <elkido@hotmail.no>
2022-10-26 14:56:42 +0200

    Bruker

--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { setTimeout } = require('timers/promises');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bruker')
 		.setDescription('Informasjon rundt bruker.'),
 	async execute(interaction) {
-		await interaction.reply(`Kommandoen kjørt av ${interaction.user.username}, som joinet ${interaction.member.joinedAt}.`);
+		await interaction.reply(`Kommandoen kjørt av ${interaction.user.username}, med id ${interaction.user.id}`);
+		await setTimeout(1000);
+		await interaction.editReply(`Som joinet ${interaction.member.joinedAt}.`);
+		console.log(interaction.member.joinedAt);
+		await setTimeout(1000);
+		await interaction.followUp(`Og har rollen ${interaction.member.roles.cache.first().name}.`);
 	},
 };
